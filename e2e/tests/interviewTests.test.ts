2e3d8f516cbe8d9c72a50854c53135008d4327c7
pennypriddy <155271919+pennypriddy@users.noreply.github.com>
2025-03-31 14:04:46 -0700

    clarify readme

--- e2e/tests/interviewTests.test.ts
+++ e2e/tests/interviewTests.test.ts
@@ -7,7 +7,7 @@ test('Should be able to click on link for Charmander and verify weaknesses', asy
   await page.getByTestId('on/off-button').click()
   await page.getByRole('button', { name: 'Click to Start' }).click()
   // Should be able to filter to filter type to a ground type pokemon that's weak to fighting, 
-  // click on graveler (id 75), and confirm the page it lands on shows the correct evolutions 
+  // click on Graveler (id 75), and confirm the page it lands on shows the correct evolutions 
   // (Geodude and Golem).
   //WRITE YOUR TEST HERE
 })
